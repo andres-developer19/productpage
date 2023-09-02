b94cfc604ea5e6fa0b026180fb252932a6dfa83b
andres.developer19 <andres.developer19@gmail.com>
2023-09-02 16:14:28 -0300

    first commit

---
+++ app/components/Route.jsx
@@ -0,0 +1,19 @@
+
+import Image from 'next/image'
+import Item from './item/Item'
+
+const fetchProducts = async () => {
+    const res = await fetch(`https://fakestoreapi.com/products`)
+    const data =  res.json()    
+    return data
+}
+
+ async function Route() {
+    const products = await fetchProducts()
+    return (
+      <Item products={products} />
+    )
+   
+}
+
+export default Route
